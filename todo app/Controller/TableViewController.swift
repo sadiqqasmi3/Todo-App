4543ba7546401ae0356c7858afe8851f996429d1
sadiq <sadiq.qasmi3@gmail.com>
2021-02-19 04:17:24 +0500

    done with user defaults , checkmarks

---
+++ todo app/Controller/TableViewController.swift
@@ -0,0 +1,132 @@
+//
+//  TableViewController.swift
+//  todo app
+//
+//  Created by sadiq qasmi on 16/02/2021.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var itemArray = [Item]()
+    //let defaults = UserDefaults.standard
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let newItem = Item()
+        newItem.title = "hhee"
+        newItem.done = true
+        itemArray.append(newItem)
+        
+        let newItem3 = Item()
+        newItem3.title = "33njj"
+        itemArray.append(newItem3)
+        
+        let newItem2 = Item()
+        newItem2.title = "22jif"
+        itemArray.append(newItem2)
+//        if let item = defaults.array(forKey: "Todo list") as? [String]{
+//            itemArray = item
+//        }
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
+        
+        return cell
+        }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row].title)
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        tableView.reloadData()
+    }
+    
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add your Todo item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { [self] (action) in
+            print("working.....")
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            itemArray.append(newItem)
+            
+            //defaults.set(itemArray, forKey: "Todo list")
+            
+            tableView.reloadData()
+        }
+        alert.addAction(action)
+        alert.addTextField { (textfield) in
+            textfield.placeholder = "Create new item"
+            textField = textfield
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
---
+++ todo app/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  todo app
+//
+//  Created by sadiq qasmi on 17/02/2021.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done = false
+}
