16beb20512df3f102dc8923db00e50461f5f6958
sadiq <sadiq.qasmi3@gmail.com>
2021-02-20 16:33:35 +0500

    done with CURD

--- todo app/Controller/TableViewController.swift
+++ todo app/Controller/TableViewController.swift
@@ -6,29 +6,21 @@
 //
 
 import UIKit
+import CoreData
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController ,UISearchBarDelegate {
     
     var itemArray = [Item]()
     //let defaults = UserDefaults.standard
     
-    let filePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Todo.plist")
+//    let filePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Todo.plist")
+//
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(filePath)
-        let newItem = Item()
-        newItem.title = "hhee"
-        newItem.done = true
-        itemArray.append(newItem)
-        
-        let newItem3 = Item()
-        newItem3.title = "33njj"
-        itemArray.append(newItem3)
-        
-        let newItem2 = Item()
-        newItem2.title = "22jif"
-        itemArray.append(newItem2)
+          print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+
         
         getData()
         
@@ -54,7 +46,7 @@ class TableViewController: UITableViewController {
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row].title)
+        print(itemArray[indexPath.row].title!)
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
@@ -71,8 +63,11 @@ class TableViewController: UITableViewController {
         let action = UIAlertAction(title: "Add", style: .default) { [self] (action) in
             print("working.....")
             
-            let newItem = Item()
+            
+
+            let newItem = Item(context: context)
             newItem.title = textField.text!
+            newItem.done = false
             
             saveData()
             
@@ -90,73 +85,28 @@ class TableViewController: UITableViewController {
     }
     
     func saveData(){
-        let encoder = PropertyListEncoder()
+        
         do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: filePath!)
+            try context.save()
         }catch{
             print(error)
         }
         
-        //defaults.set(itemArray, forKey: "Todo list")
+        
         tableView.reloadData()
     }
     func getData(){
-        if let data = try? Data(contentsOf: filePath!){
-            let decoder = PropertyListDecoder()
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)
-            }catch{
+        let request:NSFetchRequest<Item> = Item.fetchRequest()
+        do{
+           itemArray = try context.fetch(request)
+
+        }catch{
             print(error)
-            }
         }
-        
     }
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
+
+
 
 
